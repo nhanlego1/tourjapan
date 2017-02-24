0f5089c7067a9c277e91c20fca8661df305288bc
Nhan Le <nhanlego1@gmail.com>
2017-02-24 22:29:21 +0700

    update

--- wp-content/themes/skt-corp/page-host_details.php
+++ wp-content/themes/skt-corp/page-host_details.php
@@ -183,7 +183,11 @@ $post_detail_id = $_GET['post-detail'];
                     <td>1</td>
                   </tr>
                   <tr>          
-                    <td colspan="7"><b>★☆ユーザー投稿レビュー☆★</b><br><br><img src="../img/revie.jpg" alt="img"></td>
+                    <td colspan="7"><b>★☆ユーザー投稿レビュー☆★</b><br><br> <?php
+                        // If comments are open or we have at least one comment, load up the comment template
+                        if ( comments_open() || '0' != get_comments_number() )
+                            comments_template();
+                        ?></td>
                   </tr>
                 </tbody>
               </table>             
