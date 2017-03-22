0e310c2169ab909e8225136780a1a114cda4ff4a
Nhan Le <nhanlego1@gmail.com>
2017-03-22 10:24:09 +0700

    updatr

--- wp-content/themes/skt-corp/page.php
+++ wp-content/themes/skt-corp/page.php
@@ -12,12 +12,14 @@
 
 get_header();
 global $post;
-$post_slug = $post->post_name;
+$post_slug = $post->post_title;
+$bg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), $size, $icon );
+
 ?>
 
 
     <div class="header1"
-         style="height:328px;background:url(<?php echo esc_url(home_url('/')); ?>img/bg-2.jpg);background-size: cover;">
+         style="height:328px;background:url(<?php if($bg){ echo $bg[0];}else{ echo esc_url(home_url('/')).'/img/bg-2.jpg';}?>);background-size: cover;">
         <div class="title-top">
             <h2><?php if ($post_slug == 'question'): ?>Q & A<?php else: ?><?php echo $post_slug; ?><?php endif; ?>
             </h2>
